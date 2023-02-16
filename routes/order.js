41c3feea93d2f15aa24a51a616f1abd31b50dc91
wsx0305 <61773316+wsx0305405@user.noreply.github.com>
2023-02-17 01:55:24 +0800

    Restful-order_CRUD

--- routes/order.js
+++ routes/order.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-// order
 var Order = require('../controllers/order/with_order');
 
 const order = new Order();
-
-
 
 // 取得全部訂單資料
 router.get('/order',order.getAllOrder);
 // 訂單筆訂單
 router.post('/order',order.postOrderOneProduct);
 // 更新訂單資料-更新資料庫
-router.put('/order/:oid/:mid/:pid',order.updateOrder);
+router.put('/order',order.updateOrder);
 // 刪除訂單
-router.delete('/order/:oid/:mid/:pid',order.deleteOrder);
+router.delete('/order',order.deleteOrder);
 
 module.exports = router;
